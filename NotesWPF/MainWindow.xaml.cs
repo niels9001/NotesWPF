384ef8fb5456768d5cdfa2f6d5de55c91cdf0931
Niels Laute <niels.laute@live.nl>
2024-10-03 14:47:34 +0200

    Add project files.

---
+++ NotesWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Windows.AppNotifications.Builder;
+using Microsoft.Windows.AppNotifications;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotesWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var notification = new AppNotificationBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", "9813")
+                .AddText("Andrew sent you a picture")
+                .AddText("Check this out, The Enchantments in Washington!")
+                .BuildNotification();
+
+            AppNotificationManager.Default.Show(notification);
+        }
+    }
+}
---
+++ NotesWPF/Utils/TextRecognizer.cs
@@ -0,0 +1,54 @@
+﻿namespace Microsoft.Windows.AI.Imaging
+{
+    internal class TextRecognizer
+    {
+        internal static async Task<TextRecognizer> CreateAsync()
+        {
+            await Task.Delay(10);
+            return new TextRecognizer();
+        }
+
+        internal static async Task MakeAvailableAsync()
+        {
+            await Task.Delay(10);
+        }
+
+        internal async Task<RecognizedText> RecognizeTextFromImageAsync(object imageBuffer, TextRecognizerOptions options)
+        {
+            await Task.Delay(10);
+            return new RecognizedText();
+        }
+    }
+
+    internal class TextRecognizerOptions()
+    {
+
+    }
+
+    internal class ImageBuffer
+    {
+        internal static ImageBuffer CreateBufferAttachedToBitmap(SoftwareBitmap image)
+        {
+            return new ImageBuffer();
+            //TextRecognition.GetTextFromImage(null);
+        }
+    }
+}
+
+public class SoftwareBitmap();
+
+    internal class RecognizedText
+    {
+        public List<RecognizedTextLine> Lines { get; set; } = new();
+        public double ImageAngle { get; set; }
+    }
+
+    internal class RecognizedTextLine
+    {
+        public string Text { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
